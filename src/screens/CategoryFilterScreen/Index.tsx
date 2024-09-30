496b7699d228e125cf2e3130da0cf25ff98ea9d4
suzanaILIEVA <suzanaa.ilieva@gmail.com>
2024-09-30 18:04:07 +0300

    TypeFiltering created

--- src/screens/CategoryFilterScreen/Index.tsx
+++ src/screens/CategoryFilterScreen/Index.tsx
@@ -2,14 +2,16 @@ import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import CategoryFiltering from "../../components/CategoryFiltering/Index";
 import { Category } from "../../models";
+import TypeFiltering from "../../components/TypeFiltering/Index";
 
 const Index = (props: any) => {
-  const [category, setCategory] = useState<Category[]>(
+  const [category, setCategory] = useState<Category>(
     props.route.params.category
   );
   return (
     <ScrollView>
       <CategoryFiltering category={category} />
+      <TypeFiltering />
     </ScrollView>
   );
 };
